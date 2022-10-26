1e514194cbaa4f460335ea4b4596341f0b980a38
mcerutti <milucerutti@gmail.com>
2022-10-25 21:11:22 -0300

    detalles

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ const shopContent = document.getElementById("shopContent")
 const verCarrito = document.getElementById("verCarrito")
 const modalContainer = document.getElementById("modal-container")
 const productos = [  
-    {id: 1, nombre: "Sorrentinos", precio: 550, variedad: "4 quesos, calabaza, ricota", img: ""},
-    {id: 2, nombre: "Tartas", precio: 600, variedad: "verduras, jamon y queso, atun", img:""},
-    {id: 3, nombre: "Fideos", precio: 350, variedad: "puro huevo, verduras", img:""},
-    {id: 4, nombre: "Canelones", precio: 450, variedad: "choclo, jamón y queso", img:""}
+    {id: 1, nombre: "Sorrentinos", precio: 550, variedad: "4 quesos, calabaza y ricota.", img: "https://media.istockphoto.com/photos/mass-of-noodles-of-different-colors-red-yellow-and-green-to-make-picture-id1262751787?s=612x612"},
+    {id: 2, nombre: "Tartas", precio: 600, variedad: "Verduras, jamon y queso, atun.", img:"https://media.istockphoto.com/photos/quiche-lorraine-slice-picture-id461801551"},
+    {id: 3, nombre: "Fideos", precio: 350, variedad: "puro huevo, verduras", img:"https://media.istockphoto.com/photos/pasta-fettuccine-with-beef-ragout-sauce-in-black-bowl-grey-background-picture-id1215312647?s=612x612"},
+    {id: 4, nombre: "Canelones", precio: 450, variedad: "Choclo o jamón y queso", img:"https://media.istockphoto.com/photos/meat-cannelloni-sauce-bechamel-picture-id502641884"}
   ]
 let carrito = []
 
@@ -15,20 +15,21 @@ productos.forEach((product) => {
     content.innerHTML =`
     <img src="${product.img}">
     <h3>${product.nombre}</h3>
+    <h3 class:"variedad">${product.variedad}</h3>
     <p class:"price">${product.precio}
     `;  
     shopContent.append(content)
 
     let comprar = document.createElement("button")
     comprar.className = "comprar"
-    comprar.innerText = "Comprar"
+    comprar.innerText = "comprar"
     content.append(comprar)
     comprar.addEventListener("click", ()=>{
         carrito.push({
-           id: product.id,
-           img: product.img,
-           nombre: product.nombre,
-           precio: product.precio
+           id : product.id,
+           img : product.img,
+           nombre : product.nombre,
+           precio : product.precio,
         })
     })
 
@@ -45,16 +46,16 @@ verCarrito.addEventListener("click", ()=> {
     `
     modalContainer.append(modalHeader)
 
-    const modalButton = document.createElement ("h1")
-    modalButton.innerText ="x"
-    modalButton.className = "modal-header-button"
+    const modalbutton = document.createElement ("h1")
+    modalbutton.innerText ="x"
+    modalbutton.className = "modal-header-button"
 
-    modalButton.addEventListener("click", () => {
+    modalbutton.addEventListener("click", () => {
         modalContainer.style.display = "none"
     })
 
 
-    modalHeader.append(modalButton)
+    modalHeader.append(modalbutton)
 
     carrito.forEach((product) =>{
     
